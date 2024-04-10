9b68ea6727682369d819990820f1fdd26f862165
ElielSantos <35500688+ElielSantos@users.noreply.github.com>
2024-04-10 20:45:01 -0300

    Update newLinux-packges.sh

--- newLinux-packges.sh
+++ newLinux-packges.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
+############################################################
+#  2024 · ELIEL SANTOS DE JESUS · santoscyberx@gmail.com   #
+############################################################
+# pode ser rodado direto no terminal
+# -y Aceita Sim para todos os comandos $sudo
 
 # Instalar Google Chrome
 wget -q -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome.deb
-sudo apt-get install -f
+sudo apt-get install -f -y
 rm google-chrome.deb
 
 # Instalar Visual Studio Code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install code
+sudo apt-get install apt-transport-https -y
+sudo apt-get update -y
+sudo apt-get install code -y
 rm packages.microsoft.gpg
 
 # Instalar Terminator
-sudo apt-get install terminator
+sudo apt-get install terminator -y
 
 # Instalar o visualizador htop
-sudo apt install htop
+sudo apt install htop -y
 
 # Instalar GIMP
-sudo apt-get install gimp
+sudo apt-get install gimp -y
 
 # Instalar VLC
-sudo apt install vlc
+sudo apt install vlc -y
 
 # Instalar Photoscape via Wine
-sudo apt-get install wine
+sudo apt-get install wine -y
 wget https://s3.amazonaws.com/cdn.photoscape.org/psa-full.exe
 wine psa-full.exe
 rm psa-full.exe
 
 # Instalar o qbtorrent
-sudo apt install qbittorrent
+sudo apt install qbittorrent -y
 
 # Instalar CPU-X
-sudo apt-get install cpu-x
+sudo apt-get install cpu-x -y
 
 # Instalar Oracle VM VirtualBox
-sudo apt-get install virtualbox
+sudo apt-get install virtualbox -y
